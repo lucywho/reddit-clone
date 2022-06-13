997ad9521c6023268bc4ca3fbe0ee628b0772785
Lucy Toman <lucy@toman.me.uk>
2022-06-13 17:48:14 +0200

    displays posts in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
-export default function Home() {
+import Head from "next/head"
+import { getPosts } from "lib/data.js"
+import prisma from "lib/prisma"
+import Posts from "components/Posts"
+
+export default function Home({ posts }) {
     return (
-        <div className="text-blue-800 font-bold pl-10">
-            greetings earthlings
-        </div>
+        <>
+            <Head>
+                <title>Skimmdit</title>
+                <meta name="description" content="Like Reddit but Worse!" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1 className="title">Skimmdit</h1>
+            <p className="tagline">Like Reddit but Worse!</p>
+            <Posts posts={posts} />
+        </>
     )
+}
+
+export async function getServerSideProps() {
+    let posts = await getPosts(prisma)
+    posts = JSON.parse(JSON.stringify(posts))
+
+    return {
+        props: {
+            posts: posts,
+        },
+    }
 }
