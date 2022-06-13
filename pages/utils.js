7ebd929995398c0757b4bcd673cf194a40cb8520
Lucy Toman <lucy@toman.me.uk>
2022-06-13 16:25:16 +0200

    adds utilities to post & delete dummy content

---
+++ pages/utils.js
@@ -0,0 +1,48 @@
+export default function Utils() {
+    const tasks = [
+        { task: "generate_users", description: "Generate random users" },
+        {
+            task: "generate_subreddits",
+            description: "Generate random subreddits",
+        },
+        {
+            task: "add_fake_content",
+            description: "Add fake content",
+        },
+        {
+            task: "clean_database",
+            description: "Clean the database",
+        },
+    ]
+
+    const Button = ({ task }) => (
+        <div className="flex-1 mb-5">
+            <button
+                className="utilsbut"
+                onClick={async () => {
+                    await fetch("/api/utils", {
+                        body: JSON.stringify({
+                            task: task.task,
+                        }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        method: "POST",
+                    })
+                }}
+            >
+                {task.description}
+            </button>
+        </div>
+    )
+
+    return (
+        <div className="mt-10 ml-20 title">
+            <h2 className="mb-10 text-xl font-bold">Utils</h2>
+
+            {tasks.map((task, index) => (
+                <Button key={index} task={task} />
+            ))}
+        </div>
+    )
+}
