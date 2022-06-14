1fec64e76eac969e048095204cf0b8d411c72413
Lucy Toman <lucy@toman.me.uk>
2022-06-14 11:42:53 +0200

    splits up post components

--- components/Posts.js
+++ components/Posts.js
@@ -1,24 +1,4 @@
-import timeago from "lib/timeago"
-
-const Post = ({ post }) => {
-    return (
-        <div className="contentbox">
-            <div className="flex items-center text-gray-800">
-                /r/{post.subredditName} Posted by {post.author.name}{" "}
-                {timeago.format(new Date(post.createdAt))}
-            </div>
-
-            <div>
-                <p className="flex-shrink text-2xl font-bold mt-2 ">
-                    {post.title}
-                </p>
-                <p className="flex-shrink text-base font-normal width-auto mt-2">
-                    {post.content}
-                </p>
-            </div>
-        </div>
-    )
-}
+import Post from "./Post"
 
 export default function Posts({ posts }) {
     if (!posts) return null
